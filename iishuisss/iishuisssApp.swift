75e4cca61aa75cadcf1c74d79159bcdd6bd8b046
LowLife <125573358+Bogdan52003@users.noreply.github.com>
2024-05-04 19:49:24 +0300

    Initial Commit

---
+++ iishuisss/iishuisssApp.swift
@@ -0,0 +1,17 @@
+//
+//  iishuisssApp.swift
+//  iishuisss
+//
+//  Created by Bogdan Chupakhin on 04.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct iishuisssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
